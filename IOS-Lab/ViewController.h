9192c0b3f2647feef7dd29da45ed49c904cfa6ff
Keming <believe.75467@gmail.com>
2015-05-11 17:17:21 +0800

    init project

---
+++ IOS-Lab/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  IOS-Lab
+//
+//  Created by Keming on 2015/5/11.
+//  Copyright (c) 2015年 Keming. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ IOS-Lab/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  IOS-Lab
+//
+//  Created by Keming on 2015/5/11.
+//  Copyright (c) 2015年 Keming. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
