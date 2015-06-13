c0ff9cd3b9ded3715ac74077358564993620a1c3
KemingChen <believe75467@gmail.com>
2015-06-13 22:09:16 +0800

    delete unuse code

--- IOS-Lab/Human/CheckIn.m
+++ IOS-Lab/Human/CheckIn.m
@@ -28,27 +28,6 @@
     return checkIn;
 }
 
-+ (CheckIn*)checkInWithPoster:(NSInteger)identity user:(User*)user desc:(NSString*)desc imagePhoto:(UIImage*)imagePhoto
-{
-    CheckIn* checkIn = nil;
-
-    NSArray* checkIns = [CheckIn MR_findByAttribute:@"identity" withValue:@(identity) inContext:[NSManagedObjectContext MR_defaultContext]];
-
-    if (checkIns.count > 0) {
-        checkIn = [checkIns firstObject];
-    }
-    else {
-        checkIn = [CheckIn MR_createEntityInContext:[NSManagedObjectContext MR_defaultContext]];
-    }
-
-    checkIn.user = user;
-    checkIn.desc = desc;
-    checkIn.isPhotoLocalValue = true;
-    checkIn.imagePhoto = UIImagePNGRepresentation(imagePhoto);
-
-    return checkIn;
-}
-
 + (void)save
 {
     [[NSManagedObjectContext MR_defaultContext] MR_saveToPersistentStoreAndWait];
